7078e5452986088570861fd317656d9eb3d8dc5c
Yimeng(Yvonne) Wu <ywuyvonne@gmail.com>
2024-04-24 14:27:53 +0800

    Initial commit

---
+++ build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_project1_FILE E:/Projects/others/Apr21/QtApplicaiton/project1/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/project1.exe)
+set(__QT_DEPLOY_TARGET_project1_RUNTIME_DLLS E:/Application/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;E:/Application/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;E:/Application/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
